a39c28025054a4c86f0b66b878ecbfe618380037
marest94 <stojanovic1994@yahoo.com>
2020-09-04 18:07:27 +0200

    Modify SwaggerConfiguration to support LocalDateTime

--- src/main/java/eu/demeterh2020/resourceregistrymanagement/configuration/SwaggerConfiguration.java
+++ src/main/java/eu/demeterh2020/resourceregistrymanagement/configuration/SwaggerConfiguration.java
@@ -1,17 +1,26 @@
 package eu.demeterh2020.resourceregistrymanagement.configuration;
 
+import com.fasterxml.classmate.TypeResolver;
 import com.google.common.base.Predicate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import static com.google.common.base.Predicates.not;
 import static com.google.common.base.Predicates.or;
@@ -21,11 +30,20 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+    @Autowired
+    TypeResolver typeResolver;
+
     @Bean
     @ConditionalOnMissingBean
     public Docket apiSwagger2Description() {
 
         return new Docket(DocumentationType.SWAGGER_2)
+                .alternateTypeRules(
+                        AlternateTypeRules.newRule(
+                        typeResolver.resolve(List.class, LocalDateTime.class),
+                        typeResolver.resolve(List.class, Date.class), Ordered.HIGHEST_PRECEDENCE),
+                        AlternateTypeRules.newRule(typeResolver.resolve(Map.class, String.class, LocalDateTime.class),
+                                typeResolver.resolve(Map.class, String.class, Date.class), Ordered.HIGHEST_PRECEDENCE))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("eu.demeterh2020.resourceregistrymanagement"))
                 .paths(not(or(regex("/error"), regex("/actuator.*"))))
