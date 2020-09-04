29bce9abb8d1a542a23c4a9b54b23471f9466f53
marest94 <stojanovic1994@yahoo.com>
2020-09-04 18:11:15 +0200

    Add createAt field to DEHResource.java

--- src/main/java/eu/demeterh2020/resourceregistrymanagement/domain/DEHResource.java
+++ src/main/java/eu/demeterh2020/resourceregistrymanagement/domain/DEHResource.java
@@ -8,7 +8,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -18,30 +19,31 @@ public class DEHResource {
 
     @Id
     private String uid;
-    @NotNull (message = "Resource name can't be null")
+    @NotNull(message = "Resource name can't be null")
     private String name;
-    @NotNull (message = "Resource type can't be null")
+    @NotNull(message = "Resource type can't be null")
     private String type;
-    private List<String> category;
+    private List<String> category = new ArrayList<>();
     private String description;
     private String endpoint;
-    @NotNull (message = "Resource status can't be null")
+    @NotNull(message = "Resource status can't be null")
     private String status;
     private String version;
     private int maturityLevel;
-
-    @NotNull (message = "Resource owner can't be null")
+    @NotNull(message = "Resource owner can't be null")
     private String owner;
-    private List<String> tags;
+    private List<String> tags = new ArrayList<>();
     private byte[] Attachment;
     private Double rating;
-    private List<GeoJsonPoint> localisation;
+    private List<GeoJsonPoint> localisation = new ArrayList<>();
     private int accessibility;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private Date lastUpdate = new Date();
-    private List<String> dependencies;
-    private List<String> accessControlPolicies;
+    private LocalDateTime createAt;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime lastUpdate;
+    private List<String> dependencies = new ArrayList<>();
+    private List<String> accessControlPolicies = new ArrayList<>();
     private String url;
-    private List<String> billingInformation;
+    private List<String> billingInformation = new ArrayList<>();
 
 }
