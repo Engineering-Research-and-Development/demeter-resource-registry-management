94a6aa5a076874aab595fb562d41a47901f7d356
marest94 <stojanovic1994@yahoo.com>
2020-09-04 18:07:02 +0200

    Add ModelMapper

--- src/main/java/eu/demeterh2020/resourceregistrymanagement/configuration/ApplicationConfiguration.java
+++ src/main/java/eu/demeterh2020/resourceregistrymanagement/configuration/ApplicationConfiguration.java
@@ -1,25 +1,13 @@
 package eu.demeterh2020.resourceregistrymanagement.configuration;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import eu.demeterh2020.resourceregistrymanagement.filter.LoggingFilter;
 import eu.demeterh2020.resourceregistrymanagement.logging.LoggingInterceptor;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
-import org.springframework.beans.factory.annotation.Value;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -45,6 +33,11 @@ public class ApplicationConfiguration {
         filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
         return filterRegistrationBean;
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
