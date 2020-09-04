1715071ded07a47e4f21b6f999814d372469678b
marest94 <stojanovic1994@yahoo.com>
2020-09-04 18:12:57 +0200

    Add implementation for existByUid method and comment all methods

--- src/main/java/eu/demeterh2020/resourceregistrymanagement/service/DEHResourceService.java
+++ src/main/java/eu/demeterh2020/resourceregistrymanagement/service/DEHResourceService.java
@@ -12,15 +12,61 @@ import java.util.Optional;
 
 public interface DEHResourceService {
 
-    DEHResource saveDEHResource(DEHResource dehResource);
+    /**
+     * Method for storing a new DEH Resource in DB
+     *
+     * @param dehResource - DEH resource for storing
+     * @return stored DEH Resource
+     */
+    DEHResource save(DEHResource dehResource);
 
+    /**
+     * Method for updating existing DEH Resource in DB
+     *
+     * @param uid  - DEH resource uid
+     * @param data - String with modified fields in DEH Resource
+     * @return updated DEH resource
+     */
     DEHResource update(String uid, String data) throws IOException;
 
-    void deleteDEHResource(String uid);
 
-    Optional<DEHResource> findByUid(String uid);
+    /**
+     * Method for deleting DEH Resource by UID from DB
+     *
+     * @param uid - DEH resource UID
+     */
+    void deleteByUid(String uid);
 
+    /**
+     * Method for getting DEH Resource by UID from DB
+     *
+     * @param uid - DEH resource UID
+     * @return Optional DEH Resource
+     */
+    Optional<DEHResource> findOneByUid(String uid);
+
+    /**
+     * Method for checking if DEH Resources with UID exists in DB
+     *
+     * @param uid - uid - DEH resource UID
+     * @return true or false
+     */
+    boolean existByUid(String uid);
+
+    /**
+     * Method for getting all DEH Resources from DB
+     *
+     * @param pageable - object with defined page, size and sort
+     * @return page with all DEH Resources in DB
+     */
     Page<DEHResource> findAll(Pageable pageable);
 
+    /**
+     * Method for getting all DEH Resources which corresponds to specific criteria/filter from  from DB
+     *
+     * @param predicate - object with criteria
+     * @param pageable  - object with defined page, size and sort
+     * @return page with all DEH Resources in DB
+     */
     Page<DEHResource> findAllByQuery(Predicate predicate, Pageable pageable);
 }
--- src/main/java/eu/demeterh2020/resourceregistrymanagement/service/DEHResourceServiceImpl.java
+++ src/main/java/eu/demeterh2020/resourceregistrymanagement/service/DEHResourceServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 
@@ -26,39 +27,57 @@ public class DEHResourceServiceImpl implements DEHResourceService {
     @Autowired
     private CompatibilityChecker compatibilityChecker;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Loggable
-    public DEHResource saveDEHResource(DEHResource dehResource) {
+    public DEHResource save(DEHResource dehResource) {
 
+        // Check if deh resource is compatible
         if (compatibilityChecker.checkCompatibility(dehResource) == true) {
+            // Set creating time of DEH Resource
+            dehResource.setCreateAt(LocalDateTime.now());
+            // Store DEH Resource in DB
             return dehRepository.save(dehResource);
         }
+
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
+    @Loggable
     public DEHResource update(String uid, String data) throws IOException {
-
+        // Get DEH Resource from DB by UID
         DEHResource dehResource = dehRepository.findByUid(uid).orElse(null);
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new Jdk8Module());
         ObjectReader objectReader = objectMapper.readerForUpdating(dehResource);
         DEHResource updatedDehResource = objectReader.readValue(data, DEHResource.class);
+        updatedDehResource.setLastUpdate(LocalDateTime.now());
 
         return dehRepository.save(updatedDehResource);
-
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Loggable
-    public void deleteDEHResource(String uid) {
+    public void deleteByUid(String uid) {
         dehRepository.deleteById(uid);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Loggable
-    public Optional<DEHResource> findByUid(String uid) {
+    public Optional<DEHResource> findOneByUid(String uid) {
 
         Optional<DEHResource> dehResource = dehRepository.findByUid(uid);
 
@@ -66,12 +85,23 @@ public class DEHResourceServiceImpl implements DEHResourceService {
     }
 
     @Override
+    public boolean existByUid(String uid) {
+        return dehRepository.existsByUid(uid);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     @Loggable
     public Page<DEHResource> findAll(Pageable pageable) {
 
         return dehRepository.findAll(pageable);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Loggable
     public Page<DEHResource> findAllByQuery(Predicate predicate, Pageable pageable) {
