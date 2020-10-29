eb8760857452d62668d414c695326ce10b82f081
marest94 <stojanovic1994@yahoo.com>
2020-10-29 23:05:37 +0100

    Add onBeforeConvert, and modify onAfterSave methods in DehResourceMongoEventListener

--- src/main/java/eu/demeterh2020/resourceregistrymanagement/util/listener/DehResourceMongoEventListener.java
+++ src/main/java/eu/demeterh2020/resourceregistrymanagement/util/listener/DehResourceMongoEventListener.java
@@ -3,6 +3,7 @@ package eu.demeterh2020.resourceregistrymanagement.util.listener;
 import eu.demeterh2020.resourceregistrymanagement.domain.Audit;
 import eu.demeterh2020.resourceregistrymanagement.domain.DehResource;
 import eu.demeterh2020.resourceregistrymanagement.service.AuditService;
+import eu.demeterh2020.resourceregistrymanagement.service.DehResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.mapping.event.*;
 
@@ -16,16 +17,26 @@ public class DehResourceMongoEventListener extends AbstractMongoEventListener<De
     @Autowired
     AuditService auditService;
 
+    @Autowired
+    DehResourceService dehResourceService;
 
     @Override
-    public void onBeforeSave(BeforeSaveEvent<DehResource> event) {
-        super.onBeforeSave(event);
-        // TODO Extract owner from token
-        // Set date of creation of a resource
-        event.getSource().setCreateAt(LocalDateTime.now());
-        event.getSource().setLastUpdate(LocalDateTime.now());
-        event.getSource().setRating(0D);
+    public void onBeforeConvert(BeforeConvertEvent<DehResource> event) {
+        super.onBeforeConvert(event);
+
+        //TODO extract token
+        LocalDateTime now = LocalDateTime.now();
+        // Set creation date and default rating of a resource if not exists in DB
+        if (dehResourceService.existByUid(event.getSource().getUid())) {
+            event.getSource().setLastUpdate(now);
+        } else {
+            event.getSource().setLastUpdate(now);
+            event.getSource().setCreateAt(now);
+            event.getSource().setRating(0D);
+            event.getSource().setDownloadsHistory(new HashMap<>());
+        }
     }
+
 
     /* Method for creating and storing Audit data for after DEHResource is stored in DB
      */
@@ -33,16 +44,18 @@ public class DehResourceMongoEventListener extends AbstractMongoEventListener<De
     public void onAfterSave(AfterSaveEvent<DehResource> event) {
         super.onAfterSave(event);
 
-        Map<String, LocalDateTime> versions = new HashMap<String, LocalDateTime>() {
-            {
-                put(event.getSource().getVersion(), event.getSource().getCreateAt());
-            }
-        };
+        if (!auditService.existByResourceUid(event.getSource().getUid())) {
+            Map<String, LocalDateTime> versions = new HashMap<String, LocalDateTime>() {
+                {
+                    put(event.getSource().getVersion(), event.getSource().getCreateAt());
+                }
+            };
 
-        Audit newAuditResource = new Audit(event.getSource().getUid(), event.getSource().getCreateAt(),
-                event.getSource().getCreateAt(), versions, new ArrayList<>(), 0D);
+            Audit newAuditResource = new Audit(event.getSource().getUid(), event.getSource().getCreateAt(),
+                    event.getSource().getCreateAt(), versions, new ArrayList<>(), new ArrayList<>());
 
-         auditService.save(newAuditResource);
+            auditService.save(newAuditResource);
+        }
     }
 
     /* Method for deleting Audit data after DEHResource is deleted from DB
