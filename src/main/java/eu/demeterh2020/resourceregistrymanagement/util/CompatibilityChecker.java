5bdf1074ac10271ae386265a48e606f021c9c786
marest94 <stojanovic1994@yahoo.com>
2021-03-19 13:38:33 +0100

    Delete CompatibilityChecker service

--- src/main/java/eu/demeterh2020/resourceregistrymanagement/util/CompatibilityChecker.java
+++
@@ -1,8 +0,0 @@
-package eu.demeterh2020.resourceregistrymanagement.util;
-
-import eu.demeterh2020.resourceregistrymanagement.domain.DehResource;
-
-public interface CompatibilityChecker {
-
-    public boolean checkCompatibility(DehResource dehResource);
-}
