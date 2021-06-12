589ea5a9bc43add2778eb48abeb4bfc45c9625ef
marest94 <stojanovic1994@yahoo.com>
2021-06-12 10:43:32 +0200

    Update AttachmentApi

--- src/main/java/eu/demeterh2020/resourceregistrymanagement/resource/AttachmentApi.java
+++ src/main/java/eu/demeterh2020/resourceregistrymanagement/resource/AttachmentApi.java
@@ -1,8 +1,11 @@
 package eu.demeterh2020.resourceregistrymanagement.resource;
 
 import eu.demeterh2020.resourceregistrymanagement.domain.Attachment;
+import eu.demeterh2020.resourceregistrymanagement.domain.DehResource;
+import eu.demeterh2020.resourceregistrymanagement.domain.dto.JsonResponse;
 import eu.demeterh2020.resourceregistrymanagement.exception.ResourceNotFoundException;
 import eu.demeterh2020.resourceregistrymanagement.service.AttachmentService;
+import eu.demeterh2020.resourceregistrymanagement.service.DehResourceService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
@@ -17,9 +20,10 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "api/v1/attachment")
+@RequestMapping(value = "api/v1/attachments")
 public class AttachmentApi {
 
     private final static Logger log = LoggerFactory.getLogger(DehResourceApi.class);
@@ -27,19 +31,24 @@ public class AttachmentApi {
     @Autowired
     private AttachmentService attachmentService;
 
+
+    @Autowired
+    private DehResourceService dehResourceService;
+
     @Operation(hidden = true)
     @GetMapping("/{id}")
-    public Attachment getAttachment(@PathVariable String id) throws IllegalStateException {
+    public JsonResponse getAttachment(@PathVariable String id) throws IllegalStateException {
 
         log.info("getAttachment() called.");
 
         Attachment attachment = attachmentService.getAttachment(id);
 
-        return attachment;
+        return new JsonResponse(true, "Attachment found", attachment, null);
     }
 
     @Operation(hidden = true)
     @GetMapping("/content/{id}")
+    @CrossOrigin
     public void getAttachmentContent(@PathVariable String id, HttpServletResponse response) throws IllegalStateException, IOException {
 
         log.info("getAttachmentContent() called.");
@@ -48,12 +57,13 @@ public class AttachmentApi {
 
         if (attachment != null) {
             FileCopyUtils.copy(attachment.getInputStream(), response.getOutputStream());
+        } else {
+            throw new ResourceNotFoundException("Attachment with uid:" + id + " not found");
         }
-        throw new ResourceNotFoundException("Attachment with uid:" + id + " not found");
     }
 
     @Operation(hidden = true)
-    @PostMapping(path = "/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String addAttachment(@RequestParam("file") MultipartFile file) throws IOException {
 
         log.info("addAttachment() called.");
@@ -64,7 +74,7 @@ public class AttachmentApi {
     }
 
     @Operation(hidden = true)
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public void deleteAttachment(@PathVariable String id) throws IOException {
 
         log.info("deleteAttachment() called.");
@@ -72,8 +82,9 @@ public class AttachmentApi {
         attachmentService.deleteAttachmentById(id);
     }
 
+    @CrossOrigin
     @Operation(hidden = true)
-    @GetMapping(value = "/downloadFile/{id}")
+    @GetMapping(value = "/download/{id}")
     public void downloadFile(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         log.info("downloadFile() called.");
@@ -92,8 +103,25 @@ public class AttachmentApi {
             response.setHeader("Content-Disposition", "attachment;filename=\"" + fileName + "\"");
             response.addHeader("content-type", "application/octet-stream");
             IOUtils.copy(attachment.getInputStream(), response.getOutputStream());
-        }
+        } else {
 
-        throw new ResourceNotFoundException("Resource with uid:" + id + " not found");
+            throw new ResourceNotFoundException("Resource with uid:" + id + " not found");
+        }
+    }
+
+    @CrossOrigin
+    @Operation(hidden = true)
+    @GetMapping(value = "/counter/{id}")
+    public void counter(@PathVariable String id) throws Exception {
+
+        log.info("Counter() called.");
+
+        Optional<DehResource> dehResource = dehResourceService.findOneByUid(id);
+        if (dehResource.isPresent()) { // resource exist in DB
+            DehResource numberOfDownloads = dehResourceService.updateNumberOfDownloads(id);
+        } else {
+            log.error("Resource with uid:" + id + " not found");
+            throw new ResourceNotFoundException("Resource with uid:" + id + " not found");
+        }
     }
 }
