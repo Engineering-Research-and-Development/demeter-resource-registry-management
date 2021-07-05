196989e2332672d039330b50764e57c4f07aec1c
marest94 <stojanovic1994@yahoo.com>
2021-07-05 12:01:03 +0200

    Add proxy to IdmAuthenticationProvider.java

--- src/main/java/eu/demeterh2020/resourceregistrymanagement/security/IdmAuthenticationProvider.java
+++ src/main/java/eu/demeterh2020/resourceregistrymanagement/security/IdmAuthenticationProvider.java
@@ -1,7 +1,7 @@
 package eu.demeterh2020.resourceregistrymanagement.security;
 
-import eu.demeterh2020.resourceregistrymanagement.security.dto.RrmToken;
 import eu.demeterh2020.resourceregistrymanagement.security.dto.IdmToken;
+import eu.demeterh2020.resourceregistrymanagement.security.dto.RrmToken;
 import eu.demeterh2020.resourceregistrymanagement.security.dto.UserInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +10,16 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 public class IdmAuthenticationProvider implements AuthenticationProvider {
 
@@ -53,19 +58,52 @@ public class IdmAuthenticationProvider implements AuthenticationProvider {
 
         logger.debug("method getUserInfoFromToken() called.");
 
-        String idmUrl = "https://acs.bse.h2020-demeter-cloud.eu:5443/v1/auth/tokens";
+
+        String idmUrl = System.getenv("IDM_TOKEN_URL");
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-Auth-Token", token);
         headers.set("X-Subject-Token", token);
 
-        try {
-            ResponseEntity<IdmToken> response = restTemplate.exchange(idmUrl, HttpMethod.GET, new HttpEntity<>(headers), IdmToken.class);
-            return response.getBody().getUser();
+        String httpsProxy = System.getenv("HTTPS_PROXY");
+//TODO Refactor this
+        if (httpsProxy != null) {
+            logger.info("HTTPS_PROXY: " + httpsProxy);
+            logger.info("IDM_URL: " + idmUrl);
+            String[] httpsParts = httpsProxy.trim().split(":");
+//            String httpsProxyHostname = httpsParts[0]+ ":" + httpsParts[1];
+            String httpsProxyHostname = httpsParts[1].replace("//","");
+            Integer httpsProxyPort = Integer.valueOf(httpsParts[2]);
 
-        } catch (HttpClientErrorException ex) {
-            logger.error("IDM: " + ex.getStatusCode() + ", " + ex.getResponseBodyAsString());
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(httpsProxyHostname, httpsProxyPort));
+            SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+            requestFactory.setProxy(proxy);
+            RestTemplate restTemplateProxy = new RestTemplate(requestFactory);
+
+            try {
+                ResponseEntity<IdmToken> response = restTemplateProxy.exchange(idmUrl, HttpMethod.GET, new HttpEntity<>(headers), IdmToken.class);
+                return response.getBody().getUser();
+
+            } catch (HttpClientErrorException httpClientErrorException) {
+                logger.error("IDM: " + httpClientErrorException.getStatusCode() + ", " + httpClientErrorException.getResponseBodyAsString());
+            }
+
+
+            return null;
+        } else {
+
+
+            try {
+                ResponseEntity<IdmToken> response = restTemplate.exchange(idmUrl, HttpMethod.GET, new HttpEntity<>(headers), IdmToken.class);
+                return response.getBody().getUser();
+
+            } catch (HttpClientErrorException ex) {
+                logger.error("IDM: " + ex.getStatusCode() + ", " + ex.getResponseBodyAsString());
+            }
+            catch (ResourceAccessException ex){
+                logger.error("Exception, cause: " + ex.getCause() + ", message" + ex.getMessage());
+            }
+            return null;
         }
-        return null;
     }
 }
