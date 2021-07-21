bc7e204affcd1a9fcb373091f7feb7f3b96741ab
marest94 <stojanovic1994@yahoo.com>
2021-07-21 13:30:05 +0200

    Add constraints in MetricsDataDto

--- src/main/java/eu/demeterh2020/resourceregistrymanagement/domain/dto/MetricsDataDto.java
+++ src/main/java/eu/demeterh2020/resourceregistrymanagement/domain/dto/MetricsDataDto.java
@@ -1,11 +1,13 @@
 package eu.demeterh2020.resourceregistrymanagement.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import eu.demeterh2020.resourceregistrymanagement.domain.Author;
 import eu.demeterh2020.resourceregistrymanagement.domain.MetricsVolume;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,24 +17,37 @@ import java.util.List;
 @NoArgsConstructor
 public class MetricsDataDto {
 
+    @NotNull
     @JsonProperty("_id")
     private String containerId;
+    @JsonProperty("consumerId")
+    private String consumerId;
+    @JsonProperty("consumer")
+    private Author consumer;
+    @NotNull
     @JsonProperty("uptime")
     private Long uptime;
+    @NotNull
     @JsonProperty("hostname")
     private String hostname;
+    @NotNull
     @JsonProperty("ip")
     private String ip;
+    @NotNull
     @JsonProperty("image")
     private String image;
     @JsonProperty("BSE_ID")
     private String bseId;
+    @NotNull
     @JsonProperty("RRM_ID")
     private String rrmId;
+    @NotNull
     @JsonProperty("lastupdated")
     private Instant lastUpdated;
+    @NotNull
     @JsonProperty("cpu_percent")
     private List<MetricsVolume> cpuConsumption = new ArrayList<>();
+    @NotNull
     @JsonProperty("mem_percent")
     private List<MetricsVolume> memoryConsumption = new ArrayList<>();
 
