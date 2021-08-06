ecc1cf8720d6be4bed2a1fdc9192602500302789
marest94 <stojanovic1994@yahoo.com>
2021-08-06 13:27:55 +0200

    Add generics in JsonResponse

--- src/main/java/eu/demeterh2020/resourceregistrymanagement/domain/dto/JsonResponse.java
+++ src/main/java/eu/demeterh2020/resourceregistrymanagement/domain/dto/JsonResponse.java
@@ -1,14 +1,15 @@
 package eu.demeterh2020.resourceregistrymanagement.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class JsonResponse {
-
+public class JsonResponse <T, S> {
+    @Schema(example = "true")
     private boolean success;
     private String message;
-    private Object data;
-    private Object extraData;
+    private T data;
+    private S extraData;
 }
